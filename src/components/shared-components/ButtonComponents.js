9002a2bb5c24a7c8269ad469b5c13f737413ed8f
samrahanif0340 <samra.hanif@yahoo.com>
2022-03-25 13:08:34 +0500

    extra spaces removed

--- src/components/shared-components/ButtonComponents.js
+++ src/components/shared-components/ButtonComponents.js
@@ -1,72 +1,22 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye,faCircleCheck ,faBan} from '@fortawesome/free-solid-svg-icons'
-import {
-    CButton,
-    CCard,
-    CCardBody,
-    CCardHeader,
-    CCol,
-    CLink,
-    CModal,
-    CModalBody,
-    CModalFooter,
-    CModalHeader,
-    CModalTitle,
-    CPopover,
-    CRow,
-    CTooltip,
-  } from '@coreui/react'
-  import { DocsCallout, DocsExample } from 'src/components'
 
 
-
-const VenueButtons = (props) => {
-    // console.log(props)
-
-
-    
+const VenueButtons = (props) => {      
     const viewVenueDetails = (props) =>{
-
     }  
-
-    // const goForApproval = () => {
-    //     let payload = {
-    //             VenueID: props.data.VenueID,
-    //             VenueStatus: "A",
-    //             VenueRejectionComment: "",
-    //             UserID: 1
-    //     }
-    //     approveRejectService(payload)
-    // }
-
-    // const goForRejection = () => {
-    //     console.log('Rejection')
-    //     let payload = {
-    //         VenueID: props.data.VenueID,
-    //         VenueStatus: "R",
-    //         VenueRejectionComment: "",
-    //         UserID: 1
-    // }
-    // approveRejectService(payload)
-    // }
 
     const approveVenue = () =>{
         props.context.methodForApproveVenues(props.data,true);
-
     } 
 
     const rejectVenue = () =>{
         props.context.methodForRejectVenues(props.data,true);
-    }
-
-    
+    }  
  
     return (
-        <>
-       
-
-        
+        <>   
         <span className="mx-3" title="View Venue Details" onClick={viewVenueDetails}><FontAwesomeIcon icon={faEye}/></span>
             <span className="mx-3" title="Approve Venue" onClick={approveVenue}><FontAwesomeIcon icon={faCircleCheck}/></span>
             <span className="mx-3" title="Reject Venue" onClick={rejectVenue}><FontAwesomeIcon icon={faBan}/></span>
